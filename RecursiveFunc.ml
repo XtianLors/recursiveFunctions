a7e22bc43a064013a85dbd1f5ff832387b516727
XtianLors <crisdalori@gmail.com>
2024-06-19 14:06:03 -0600

    Added Factorial function and quotes.

--- RecursiveFunc.ml
+++ RecursiveFunc.ml
@@ -1,9 +1,23 @@
 
 
 
-
+(*Fibonacci sequence. *)
 let rec fib n =
     if n < 2 then n else fib (n - 1) + fib (n - 2);;
 
-let rec ConSum x = if x < 1 then x else x + ConSum(x-1);;
 
+(*Consecutive sum of numbers. *)
+let rec conSum x = if x < 1 then x else x + ConSum(x-1);;
+
+
+(*Factorial Multiplication. Taylor, C. (2012)*)
+let rec factorial n =
+   if n == 0 then 1 else n * factorial(n-1);;
+
+
+(*References*)
+(*
+Taylor, C. (2012). Why does ocaml need both "let" and "let rec"? [duplicate] [Online]. Stackoverflow.com. Gathered in 19 June 2024 from https://stackoverflow.com/questions/9325888/why-does-ocaml-need-both-let-and-let-rec
+
+
+*)
